63319c897d65fc33370b4d143f29b437ace2dbba
afkhaertdinov <iksendt@gmail.com>
2024-02-21 03:51:11 +0300

    Привет Мир!

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,16 +4,6 @@ public class Hello {
     }
 
     public static void main(String[] args) {
-        //sayHello();
-        int zadDays = 10000;
-        int years = zadDays / 365;
-        int months = zadDays % 365 / 30;
-        int days = zadDays % 365 % 30;
-
-        System.out.println("в " + zadDays + " дней входит:");
-        System.out.println(years + " лет");
-        System.out.println(months + " месяцев");
-        System.out.println(days + " дней");
-        System.out.println("Summ: " + (years*365 + months*30 + days));
+        sayHello();
     }
 }
