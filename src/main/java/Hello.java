ee15f140f196e52cefc28c15d4167989f1b435c5
afkhaertdinov <iksendt@gmail.com>
2024-02-25 23:50:17 +0300

    Чистый новый проект

--- src/main/java/Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello {
-    public static void sayHello() {
-        System.out.println("Привет Мир!");
-    }
-
-    public static void main(String[] args) {
-        sayHello();
-    }
-}
